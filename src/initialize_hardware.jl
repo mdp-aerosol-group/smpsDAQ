e9416b61ff4f3610fd46613ac6633c4a7dfb7f97
Sarah Petters <sarah.petters@ucr.edu>
2024-04-19 10:04:53 -0700

    update hardware config

--- src/initialize_hardware.jl
+++ src/initialize_hardware.jl
@@ -1,7 +1,13 @@
-(@isdefined HANDLE) || (HANDLE = openUSBConnection(-1))
-caliInfo = getCalibrationInformation(HANDLE)
-(powerSupply == :Ultravolt) 
-(caliInfoTdac = getTdacCalibrationInformation(HANDLE, 2))
+HANDLE = openUSBConnection(1)     # Internal Labjack connected to Ultravolt
+HANDLE1 = openUSBConnection(3)    # External Labjack connected to blue rack
+caliInfo = getCalibrationInformation(HANDLE)  
+caliInfo1 = getCalibrationInformation(HANDLE1)
+caliInfoTdac1 = getTdacCalibrationInformation(HANDLE1, 0) # TickDAC on outside
+
+# Rack has 4 power supplies, two Ultravolt - LJ(1) and two Spellman - LJ(3)
+powerSupply1 == :Ultravolt   
+powerSupply2 == :Ultravolt 
+powerSupply3 == :Spellman 
+powerSupply4 == :Spellman 
 
 CPCType1, flowRate1, port1 = configure_serial_port(1)
-CPCType2, flowRate2, port2 = configure_serial_port(2)
