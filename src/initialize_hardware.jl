64397cb596606a98db841fc2b5d7a714a42fee0b
Sarah Petters <sarah.petters@ucr.edu>
2024-04-19 10:11:10 -0700

    update hardware config

--- src/initialize_hardware.jl
+++ src/initialize_hardware.jl
@@ -5,9 +5,9 @@ caliInfo1 = getCalibrationInformation(HANDLE1)
 caliInfoTdac1 = getTdacCalibrationInformation(HANDLE1, 0) # TickDAC on outside
 
 # Rack has 4 power supplies, two Ultravolt - LJ(1) and two Spellman - LJ(3)
-powerSupply1 == :Ultravolt   
-powerSupply2 == :Ultravolt 
-powerSupply3 == :Spellman 
-powerSupply4 == :Spellman 
+powerSupply1 = :Ultravolt   
+powerSupply2 = :Ultravolt 
+powerSupply3 = :Spellman 
+powerSupply4 = :Spellman 
 
 CPCType1, flowRate1, port1 = configure_serial_port(1)
